6f7fe87144ce313e7b15cb6a9335ae89ab7a1178
spades856 <67428210+spades856@users.noreply.github.com>
2020-06-26 13:44:34 -0400

    Add files via upload

---
+++ NET Lab 1/NET Lab 1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace NET_Lab_1
+{
+    partial class frmUnitAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblUnitPrompt = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.txtUnits = new System.Windows.Forms.TextBox();
+            this.txtDailyUnits = new System.Windows.Forms.TextBox();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(389, 357);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(159, 47);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "E&xit";
+            this.toolTip1.SetToolTip(this.btnExit, "Exit the application.");
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReset.Location = new System.Drawing.Point(221, 357);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(146, 48);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "&Reset";
+            this.toolTip1.SetToolTip(this.btnReset, "Clear all inputs from the form.");
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(55, 356);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(147, 49);
+            this.btnEnter.TabIndex = 5;
+            this.btnEnter.Text = "&Enter";
+            this.toolTip1.SetToolTip(this.btnEnter, "Input your unit amounts.");
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblUnitPrompt
+            // 
+            this.lblUnitPrompt.AutoSize = true;
+            this.lblUnitPrompt.Location = new System.Drawing.Point(158, 43);
+            this.lblUnitPrompt.Name = "lblUnitPrompt";
+            this.lblUnitPrompt.Size = new System.Drawing.Size(44, 17);
+            this.lblUnitPrompt.TabIndex = 0;
+            this.lblUnitPrompt.Text = "&Units:";
+            this.lblUnitPrompt.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Location = new System.Drawing.Point(367, 43);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(41, 17);
+            this.lblDay.TabIndex = 2;
+            this.lblDay.Text = "Day: ";
+            this.toolTip1.SetToolTip(this.lblDay, "Next entry is for this day.");
+            // 
+            // txtUnits
+            // 
+            this.txtUnits.Location = new System.Drawing.Point(221, 40);
+            this.txtUnits.Name = "txtUnits";
+            this.txtUnits.Size = new System.Drawing.Size(100, 22);
+            this.txtUnits.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.txtUnits, "Enter your unit amounts here.");
+            // 
+            // txtDailyUnits
+            // 
+            this.txtDailyUnits.Location = new System.Drawing.Point(118, 89);
+            this.txtDailyUnits.MaximumSize = new System.Drawing.Size(354, 169);
+            this.txtDailyUnits.MinimumSize = new System.Drawing.Size(354, 169);
+            this.txtDailyUnits.Multiline = true;
+            this.txtDailyUnits.Name = "txtDailyUnits";
+            this.txtDailyUnits.ReadOnly = true;
+            this.txtDailyUnits.Size = new System.Drawing.Size(354, 169);
+            this.txtDailyUnits.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.txtDailyUnits, "Entries that you\'ve inputted");
+            // 
+            // lblResults
+            // 
+            this.lblResults.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResults.Location = new System.Drawing.Point(118, 265);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(354, 75);
+            this.lblResults.TabIndex = 4;
+            this.lblResults.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.lblResults, "Results");
+            // 
+            // frmUnitAverage
+            // 
+            this.AcceptButton = this.btnEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnReset;
+            this.ClientSize = new System.Drawing.Size(598, 438);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.txtDailyUnits);
+            this.Controls.Add(this.txtUnits);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblUnitPrompt);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnEnter);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(616, 485);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(616, 485);
+            this.Name = "frmUnitAverage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Average of Units Shipped";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnExit;
+        internal System.Windows.Forms.Button btnReset;
+        internal System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblUnitPrompt;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox txtUnits;
+        private System.Windows.Forms.TextBox txtDailyUnits;
+        private System.Windows.Forms.Label lblResults;
+    }
+}
+
---
+++ NET Lab 1/NET Lab 1/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/************************************************************
+ * Name: Jacob De Lio
+ * Student #: 100588162
+ * Date Created: May 29th, 2020
+ * 
+ * Purpose: To create a windows form that calculates the
+ * average of the alotted units shipped over the course of
+ * 1-7 weeks.
+ * *********************************************************/
+
+namespace NET_Lab_1
+{
+    public partial class frmUnitAverage : Form
+    {
+        /***************************
+        *       DECLARATIONS       *
+        ***************************/
+
+        //Counter for later processes
+        int generalCounter = 0;
+
+        //Stores what day # it is
+        int dayCount = 0;
+
+        //Array for containing the inputted units shipped
+        int[] unitAmount = new int[8];
+
+        //The sum of all the units shipped. Used for calculation of average
+        double unitSum = 0;
+
+        //The final average of the units shipped
+        double unitAverage = 0;
+
+        
+
+        public frmUnitAverage()
+        {
+            InitializeComponent();
+
+            //Sets the day count to 1 from the get-go
+            lblDay.Text = "Day 1";
+        }
+
+        //User presses the Exit button
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Closes the application
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //User presses the Reset button
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //Clears the units input box
+            txtUnits.Clear();
+
+            //Clears the previously recorded units
+            txtDailyUnits.Clear();
+
+            //Resets day counter to 0
+            dayCount = 0;
+
+            //Resets the day counter back to day 1
+            lblDay.Text = "Day 1";
+
+            //Resets the final average variable
+            unitAverage = 0;
+
+            //Resets the sum variable
+            unitSum = 0;
+
+            //Clears the input textbox
+            txtUnits.Text = "";
+
+            //Resets the counter
+            generalCounter = 0;
+
+            //Makes the enter button pressable again
+            btnEnter.Enabled = true;
+
+            //Clears the final results label
+            lblResults.Text = "";
+
+            //Clears the input history textbox
+            txtDailyUnits.Text = "";
+
+            //Lets you make inputs again
+            txtUnits.ReadOnly = false;
+
+            //Puts the focus on the inputs box
+            txtUnits.Focus();
+        }
+
+        //User presses the Enter button
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+
+            /***************************
+            *          INPUTS          *
+            ***************************/
+
+            //Checks to see if user entry is a whole number
+            if(int.TryParse(txtUnits.Text, out int valCheck))
+            {
+                //Check to see if user input is OUTSIDE the valid range
+                if ((valCheck < 0) || (valCheck > 5000))
+                {
+                    //Error message
+                    lblResults.Text = "ERROR unit amount must be between 0 and 5000. Please try again.";
+
+                    //Clears the input textbox
+                    txtUnits.Text = "";
+
+                    //Focus' back onto the input textbox
+                    txtUnits.Focus();
+                }
+                //User has entered a valid entry. Continue with program
+                else
+                {
+                    //Makes the days count up per input
+                    dayCount++;
+
+                    //Makes a counter count up for later use
+                    generalCounter++;
+
+                    unitAmount[dayCount] = valCheck;
+
+                    //Adds the inputted amounts to the array for later calculations
+                    unitAmount[dayCount] = Convert.ToInt32(txtUnits.Text);
+
+                    //Adds inputs to the input history box, making new lines for each entry
+                    txtDailyUnits.Text += Convert.ToString(unitAmount[generalCounter]) + Environment.NewLine;
+
+                    //Adds up all the units shipped
+                    unitSum += Convert.ToDouble(txtUnits.Text);
+
+                    //Clears the input textbox
+                    txtUnits.Text = "";
+
+                    //Formula for determining the overall average
+                    unitAverage = unitSum / 7.0;
+
+                    //Rounds results to the nearest decimal
+                    Math.Round(unitAverage);
+                }
+            }
+            //User has entered a non whole number. Displays error message
+            else
+            {
+                //Error message
+                lblResults.Text = "ERROR unit amounts can only be whole numbers. Please try again.";
+
+                //Clears the input textbox
+                txtUnits.Text = "";
+
+                //Focus' back onto the input textbox
+                txtUnits.Focus();
+            }
+
+            //If statement for if the final 7th day is reached
+            if (dayCount == 7)
+            {
+                //Updates the day counter
+                lblDay.Text = "Day " + dayCount;
+
+                //Disables the Enter button
+                btnEnter.Enabled = false;
+
+                //Prevents user from inputting anything else
+                txtUnits.ReadOnly = true;
+
+                //Displays the final results
+                lblResults.Text = "Average units per day: " + Convert.ToString(Math.Round(unitAverage, 2));
+            }
+            //For if it is not the final 7th day
+            else
+            {
+                //Updates the day counter to the next day
+                lblDay.Text = "Day " + Convert.ToInt32(dayCount + 1);
+            }
+
+        }
+    }
+}
